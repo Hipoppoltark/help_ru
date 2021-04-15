19ff9d3aefcac9554e96930602a61b6c3604cec1
Liza <poltavina.liza@yandex.ru>
2021-04-15 16:06:37 +0300

    update ---

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask import url_for, request, render_template, session, abort
 from flask import redirect, make_response, jsonify
 
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from flask_migrate import Migrate
 from flask_restful import reqparse, abort, Api, Resource
 
 from flask_socketio import SocketIO, send
@@ -388,15 +387,3 @@ def search():
     param['records'] = records
     return render_template('index.html', **param)
 
-
-if __name__ == '__main__':
-    db = db_session.global_init("postgres://edfgqwwnxaatqk:21da450b0900c800d3db53aa7e48060cb58227e10a07d449e592"
-                           "9ef9f4819b7b@ec2-52-1-115-6.compute-1.amazonaws.com:5432/djjunfccgq5gk")
-    migrate = Migrate(app, db)
-    # для списка объектов
-    api.add_resource(record_resources.RecordListResource, '/api/records')
-
-    # для одного объекта
-    api.add_resource(record_resources.RecordResource, '/api/record/<int:record_id>')
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
